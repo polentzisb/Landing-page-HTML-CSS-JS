46f5e35fcec916b32c992b10c10e6cc7ce4a74ef
Polentzi Saavedra <75914262+polentzisb@users.noreply.github.com>
2023-06-12 19:53:58 -0400

    Add files via upload

---
+++ static/js/logic.js
@@ -0,0 +1,12 @@
+function saludo() {
+  alert("Hola bienvenido a James Consulting");
+}
+
+function remove() {
+  const element = document.getElementById("content_info");
+  element.remove();
+}
+
+function changeColor() {
+  document.getElementById("prbg").style.backgroundColor = "yellow";
+}
